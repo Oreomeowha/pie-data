4162e1b5c70dbcd446ec7da36126762cb9518c8c
Oreomeowha <thiphyrakstechphn@gmail.com>
2022-11-27 16:51:23 +0700

    local project

---
+++ app.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt 
+lst = [14513,23145,23134] 
+lab = ['x','y','z'] 
+plt.pie(lst,labels=lab,colors=['yellow','orange','purple'],autopct='%.1f%%')  
+plt.title('babybro')
+plt.show() 
